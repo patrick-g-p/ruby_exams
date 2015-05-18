e9a105b2280d29e5a36b8ab237f636668f287600
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-18 17:35:23 -0400

    Inital commit. Updated README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyExams::Application.config.secret_key_base = '706a4f23f7c6608c440ac8ddb37d072e84273f1c623bf56b2bfd27778fdd466ba1431197c12025e7647cc37b169b3d2bc3a20f01cf43ec5d0b6bd0f193b4aa0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyExams::Application.config.session_store :cookie_store, key: '_ruby_exams_session'
